dcec8e97eacf2defa3b6d3d538be0c3291630bed
hyuseinleshov <hyusein.leshov@gmail.com>
2025-06-27 19:52:30 +0300

    feat: add base entity

---
+++ src/main/java/com/habittracker/api/core/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.habittracker.api.core.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(updatable = false, nullable = false)
+    private UUID id;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private Instant createdAt;
+}
