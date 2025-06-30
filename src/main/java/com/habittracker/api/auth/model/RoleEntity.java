b51c53621359deb7af80b2adee19ebaab4990098
hyuseinleshov <hyusein.leshov@gmail.com>
2025-06-30 23:51:01 +0300

    feat: add equals and hashCode implementation by Lombok
    I use only the type field because it is unique and immutable. This approach allows you to compare entities before they are persisted (when the ID is still null).

--- src/main/java/com/habittracker/api/auth/model/RoleEntity.java
+++ src/main/java/com/habittracker/api/auth/model/RoleEntity.java
@@ -2,6 +2,7 @@ package com.habittracker.api.auth.model;
 
 import com.habittracker.api.core.entity.BaseEntity;
 import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,8 +10,10 @@ import lombok.Setter;
 @Table(name = "roles")
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
 public class RoleEntity extends BaseEntity {
 
+  @EqualsAndHashCode.Include
   @Enumerated(EnumType.STRING)
   @Column(nullable = false, unique = true)
   private RoleType type;
