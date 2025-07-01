21ac9c40d39709b6cc675611f21c024ec705468b
Shenol Shengov <shenolshengov41@gmail.com>
2025-07-02 01:02:23 +0300

    feat: add validation to jwt properties

--- src/main/java/com/habittracker/api/security/jwt/config/JwtProperties.java
+++ src/main/java/com/habittracker/api/security/jwt/config/JwtProperties.java
@@ -1,18 +1,24 @@
 package com.habittracker.api.security.jwt.config;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.Duration;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 @Getter
 @Setter
 @ConfigurationProperties(prefix = "jwt")
+@Validated
 public class JwtProperties {
 
-  private String secret;
-  private String issuer;
-  private Duration expirationDuration;
-  private Duration notBeforeLeewayDuration;
-  private Integer clockSkewSeconds;
+  @NotBlank private String secret;
+  @NotBlank private String issuer;
+  @NotNull private Duration expirationDuration;
+  @NotNull private Duration notBeforeLeewayDuration;
+
+  @NotNull @Min(1) private Integer clockSkewSeconds;
 }
