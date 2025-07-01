74bc9aa86e1bcd600f24faf95ff204dc6a6e6fa2
Shenol Shengov <shenolshengov41@gmail.com>
2025-07-01 11:39:03 +0300

    test: introduce JwtTestUtils for reusable JWT token creation in tests

---
+++ src/test/java/com/habittracker/api/security/jwt/utils/JwtTestUtils.java
@@ -0,0 +1,32 @@
+package com.habittracker.api.security.jwt.utils;
+
+import io.jsonwebtoken.Jwts;
+
+import javax.crypto.SecretKey;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+public final class JwtTestUtils {
+
+    private final SecretKey key;
+
+    public JwtTestUtils(SecretKey key) {
+        this.key = key;
+    }
+
+    public String generateTestToken(String subject, String issuer, Instant expiration, Instant notBefore) {
+        return Jwts.builder()
+                .subject(subject)
+                .issuer(issuer)
+                .expiration(Date.from(expiration))
+                .notBefore(Date.from(notBefore)).
+                signWith(key).compact();
+    }
+
+    public String generateTestToken(String subject, String issuer) {
+        Instant now = Instant.now();
+        return generateTestToken(subject, issuer, now.plus(5, ChronoUnit.MINUTES),
+                now.minus(5, ChronoUnit.SECONDS));
+    }
+}
