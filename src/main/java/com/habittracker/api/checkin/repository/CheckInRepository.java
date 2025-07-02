193c6f93c7f68514f00d48ed45861cf0f3802b3b
Shenol Shengov <shenolshengov41@gmail.com>
2025-07-02 12:55:18 +0300

    refactor: format the code

--- src/main/java/com/habittracker/api/checkin/repository/CheckInRepository.java
+++ src/main/java/com/habittracker/api/checkin/repository/CheckInRepository.java
@@ -2,8 +2,6 @@ package com.habittracker.api.checkin.repository;
 
 import com.habittracker.api.checkin.model.CheckInEntity;
 import java.util.UUID;
-
-import com.habittracker.api.checkin.model.CheckInEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CheckInRepository extends JpaRepository<CheckInEntity, UUID> {}
--- src/main/java/com/habittracker/api/habit/repository/HabitRepository.java
+++ src/main/java/com/habittracker/api/habit/repository/HabitRepository.java
@@ -2,8 +2,6 @@ package com.habittracker.api.habit.repository;
 
 import com.habittracker.api.habit.model.HabitEntity;
 import java.util.UUID;
-
-import com.habittracker.api.habit.model.HabitEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HabitRepository extends JpaRepository<HabitEntity, UUID> {}
