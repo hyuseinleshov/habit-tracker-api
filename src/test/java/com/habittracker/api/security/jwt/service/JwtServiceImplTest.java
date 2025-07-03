c2c8800d1202c4f9111e86124888c6cf1ca9befd
hyuseinleshov <hyusein.leshov@gmail.com>
2025-07-03 18:31:28 +0300

    cleanup: unnecessary comments

--- src/test/java/com/habittracker/api/security/jwt/service/JwtServiceImplTest.java
+++ src/test/java/com/habittracker/api/security/jwt/service/JwtServiceImplTest.java
@@ -64,53 +64,6 @@ public class JwtServiceImplTest {
     return Duration.between(start, end).abs().toSeconds();
   }
 
-  //  @Test
-  //  public void generateToken_shouldBuildCorrectTokenWithTimestampsAndClaims() {
-  //    final JwtBuilder spyBuilder = spy(Jwts.builder());
-  //    final JwtBuilder.BuilderHeader builderHeader = mock(JwtBuilder.BuilderHeader.class);
-  //    final ArgumentCaptor<Date> dateCaptor = ArgumentCaptor.forClass(Date.class);
-  //    setUpJwtProperties();
-  //
-  //    try (MockedStatic<Jwts> jwtsMockedStatic = mockStatic(Jwts.class)) {
-  //      jwtsMockedStatic.when(Jwts::builder).thenReturn(spyBuilder);
-  //
-  //      doReturn(builderHeader).when(spyBuilder).header();
-  //      when(builderHeader.and()).thenReturn(spyBuilder);
-  //      when(builderHeader.type(anyString())).thenReturn(builderHeader);
-  //
-  //      doReturn(spyBuilder).when(spyBuilder).subject(any());
-  //      doReturn(spyBuilder).when(spyBuilder).issuer(any());
-  //      doReturn(spyBuilder).when(spyBuilder).expiration(any());
-  //      doReturn(spyBuilder).when(spyBuilder).notBefore(any());
-  //      doReturn(spyBuilder).when(spyBuilder).issuedAt(any());
-  //
-  //      totest.generateToken(TEST_SUBJECT);
-  //
-  //      Instant now = Instant.now();
-  //
-  //      verify(spyBuilder).subject(TEST_SUBJECT);
-  //      verify(spyBuilder).issuer(TEST_ISSUER);
-  //      verify(spyBuilder).issuedAt(dateCaptor.capture());
-  //      verify(spyBuilder).expiration(dateCaptor.capture());
-  //      verify(spyBuilder).notBefore(dateCaptor.capture());
-  //      verify(spyBuilder).signWith(TEST_SECRET_KEY);
-  //
-  //      List<Instant> captureTimes =
-  // dateCaptor.getAllValues().stream().map(Date::toInstant).toList();
-  //      Instant issueAt = captureTimes.get(0);
-  //      Instant expiration = captureTimes.get(1);
-  //      Instant notBefore = captureTimes.get(2);
-  //
-  //      assertTrue(Duration.between(now, issueAt).abs().toSeconds() <= TOLERANCE_SECONDS);
-  //      assertTrue(
-  //          Duration.between(now, expiration).minus(EXPIRATION_DURATION).abs().toSeconds()
-  //              <= TOLERANCE_SECONDS);
-  //      assertTrue(
-  //          Duration.between(now, notBefore).plus(NOT_BEFORE_LEEWAY_DURATION).abs().toSeconds()
-  //              <= TOLERANCE_SECONDS);
-  //    }
-  //  }
-
   @ParameterizedTest
   @MethodSource("com.habittracker.api.security.jwt.utils.JwtTestUtils#getInvalidTokens")
   public void isValid_shouldReturnExpectedResult_forGivenTokens(String token) {
