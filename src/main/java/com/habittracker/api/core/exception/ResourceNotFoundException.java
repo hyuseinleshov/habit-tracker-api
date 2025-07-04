2f7cad27e5a52aa6a3ada9789b00dc46c9ce9cb6
Shenol Shengov <shenolshengov41@gmail.com>
2025-07-04 19:26:25 +0300

    feat: add custom resource not found exception

---
+++ src/main/java/com/habittracker/api/core/exception/ResourceNotFoundException.java
@@ -0,0 +1,17 @@
+package com.habittracker.api.core.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+  public ResourceNotFoundException() {
+    super(
+        "The specific resource you requested could not be found. It may have been moved, deleted, or never existed.");
+  }
+
+  public ResourceNotFoundException(String message) {
+    super(message);
+  }
+}
