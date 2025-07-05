af85dae41e9c2c154202302407207a2a72a41855
hyuseinleshov <hyusein.leshov@gmail.com>
2025-07-05 13:44:40 +0300

    feat: add eager loading to the roles relationship
    This ensures that when a UserEntity is loaded, its roles collection is also loaded immediately, preventing the LazyInitializationException.

--- src/main/java/com/habittracker/api/auth/model/UserEntity.java
+++ src/main/java/com/habittracker/api/auth/model/UserEntity.java
@@ -19,7 +19,7 @@ public class UserEntity extends BaseEntity {
   @Column(nullable = false)
   private String password;
 
-  @ManyToMany
+  @ManyToMany(fetch = FetchType.EAGER)
   @JoinTable(
       name = "users_roles",
       joinColumns = @JoinColumn(name = "user_id"),
