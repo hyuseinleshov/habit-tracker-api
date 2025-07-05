fcac4b9f48faffc7bbac1dad8e02a47cf5d85e7e
hyuseinleshov <hyusein.leshov@gmail.com>
2025-07-05 10:09:17 +0300

    feat: add validations for AuthRequest

--- src/main/java/com/habittracker/api/auth/dto/AuthRequest.java
+++ src/main/java/com/habittracker/api/auth/dto/AuthRequest.java
@@ -1,5 +1,8 @@
 package com.habittracker.api.auth.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,6 +11,11 @@ import lombok.Setter;
 @Setter
 @Builder
 public class AuthRequest {
+  @NotBlank(message = "Email is required")
+  @Email(message = "Email must be valid")
   private String email;
+  
+  @NotBlank(message = "Password is required")
+  @Size(min = 6, message = "Password must be at least 6 characters")
   private String password;
 }
