ee4e97de5864775634a45b706ea6f03753954baf
Shenol Shengov <shenolshengov41@gmail.com>
2025-07-05 11:55:33 +0300

    feat: add optionals map of errors in api error response

--- src/main/java/com/habittracker/api/core/exception/ApiError.java
+++ src/main/java/com/habittracker/api/core/exception/ApiError.java
@@ -1,14 +1,38 @@
 package com.habittracker.api.core.exception;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.servlet.http.HttpServletRequest;
 import java.time.Instant;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ApiError(
-    Instant timestamp, Integer status, String error, String message, String path) {
+    Instant timestamp,
+    Integer status,
+    String error,
+    String message,
+    String path,
+    Map<String, String> errors) {
+
+  public static ApiError from(
+      String message, HttpStatus status, HttpServletRequest request, Map<String, String> errors) {
+    return new ApiError(
+        Instant.now(),
+        status.value(),
+        status.getReasonPhrase(),
+        message,
+        request.getRequestURI(),
+        errors);
+  }
 
   public static ApiError from(String message, HttpStatus status, HttpServletRequest request) {
     return new ApiError(
-        Instant.now(), status.value(), status.getReasonPhrase(), message, request.getRequestURI());
+        Instant.now(),
+        status.value(),
+        status.getReasonPhrase(),
+        message,
+        request.getRequestURI(),
+        null);
   }
 }
