3b0d656d1d91830525f3a1a209193ec94c9e6ee2
hyuseinleshov <hyusein.leshov@gmail.com>
2025-07-06 11:10:04 +0300

    fix: wrong id type for RoleRepository

--- src/main/java/com/habittracker/api/auth/repository/RoleRepository.java
+++ src/main/java/com/habittracker/api/auth/repository/RoleRepository.java
@@ -2,10 +2,10 @@ package com.habittracker.api.auth.repository;
 
 import com.habittracker.api.auth.model.RoleEntity;
 import com.habittracker.api.auth.model.RoleType;
+import java.util.Optional;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
+public interface RoleRepository extends JpaRepository<RoleEntity, UUID> {
   Optional<RoleEntity> findByType(RoleType type);
 }
