32068aa814eeaecd4caeb06e52d33ae13178893d
hyuseinleshov <hyusein.leshov@gmail.com>
2025-07-07 12:58:07 +0300

    test: add the AutoConfigureMockMvc annotation to BaseIntegrationTest

--- src/test/java/com/habittracker/api/common/BaseIntegrationTest.java
+++ src/test/java/com/habittracker/api/common/BaseIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.habittracker.api.common;
 
 import com.habittracker.api.config.TestConfig;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
@@ -8,6 +9,7 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
 @ActiveProfiles("test")
 @Import(TestConfig.class)
+@AutoConfigureMockMvc
 public abstract class BaseIntegrationTest {
   // Common test utilities can be added here
 }
