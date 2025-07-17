d67b476bef95c7badedbbe1ca7ba98adb7dbaa5a
hyuseinleshov <hyusein.leshov@gmail.com>
2025-07-17 20:34:49 +0300

    feat: add RefreshTokenRepository

---
+++ src/main/java/com/habittracker/api/auth/repository/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package com.habittracker.api.auth.repository;
+
+import com.habittracker.api.auth.model.RefreshTokenEntity;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshTokenEntity, UUID> {
+  Optional<RefreshTokenEntity> findByToken(String token);
+
+  void deleteByToken(String token);
+}
