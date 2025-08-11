9749ad6ba5dea9014f55be9015e8a0063f8adb1d
hyuseinleshov <hyusein.leshov@gmail.com>
2025-08-11 22:39:21 +0300

    test: update Testcontainers configuration to include Redis

--- src/test/java/com/habittracker/api/config/TestcontainersConfig.java
+++ src/test/java/com/habittracker/api/config/TestcontainersConfig.java
@@ -4,20 +4,32 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestcontainersConfig {
   private static final PostgreSQLContainer<?> postgres =
       new PostgreSQLContainer<>(DockerImageName.parse("postgres:15-alpine"));
+  
+  private static final GenericContainer<?> redis =
+      new GenericContainer<>(DockerImageName.parse("redis:7-alpine"))
+          .withExposedPorts(6379);
 
   static {
     postgres.start();
+    redis.start();
   }
 
   @Bean
   @ServiceConnection
   public PostgreSQLContainer<?> postgresContainer() {
     return postgres;
+  }
+
+  @Bean
+  @ServiceConnection
+  public GenericContainer<?> redisContainer() {
+    return redis;
   }
 }
