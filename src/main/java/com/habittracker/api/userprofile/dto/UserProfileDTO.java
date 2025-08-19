1b5dbaf012274a3b41e2c3659f97b3403f7020b1
Shenol Shengov <shenolshengov41@gmail.com>
2025-08-19 19:18:11 +0300

    fix: change way how I should user profile info

--- src/main/java/com/habittracker/api/userprofile/dto/UserProfileDTO.java
+++ src/main/java/com/habittracker/api/userprofile/dto/UserProfileDTO.java
@@ -1,15 +1,12 @@
 package com.habittracker.api.userprofile.dto;
 
-import static com.habittracker.api.auth.utils.AuthConstants.*;
-import static com.habittracker.api.userprofile.constants.UserProfileConstants.*;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.habittracker.api.core.validations.annotations.ValidTimezone;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import org.hibernate.validator.constraints.Length;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
+import static com.habittracker.api.userprofile.constants.UserProfileConstants.*;
+
 public record UserProfileDTO(
     @Length(max = 50, message = INVALID_FIRST_NAME_MESSAGE) String firstName,
     @Length(max = 50, message = INVALID_LAST_NAME_MESSAGE) String lastName,
