58939fa69425b4e1c7d78dd72aadb99bc3737d17
hyuseinleshov <hyusein.leshov@gmail.com>
2025-08-30 19:03:35 +0300

    feat: add HabitMapper with appropriate configuration

---
+++ src/main/java/com/habittracker/api/habit/mapper/HabitMapper.java
@@ -0,0 +1,13 @@
+package com.habittracker.api.habit.mapper;
+
+import com.habittracker.api.habit.dto.HabitResponse;
+import com.habittracker.api.habit.model.HabitEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface HabitMapper {
+
+    @Mapping(target = "frequency", expression = "java(com.habittracker.api.habit.model.Frequency.DAILY)")
+    HabitResponse toResponse(HabitEntity entity);
+}
