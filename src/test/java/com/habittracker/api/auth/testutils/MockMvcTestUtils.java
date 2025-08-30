02bdc6e57931529550ff5a24b33c39cedda92602
hyuseinleshov <hyusein.leshov@gmail.com>
2025-08-30 19:59:42 +0300

    test: add more utility methods for MockMvc

--- src/test/java/com/habittracker/api/auth/testutils/MockMvcTestUtils.java
+++ src/test/java/com/habittracker/api/auth/testutils/MockMvcTestUtils.java
@@ -2,6 +2,7 @@ package com.habittracker.api.auth.testutils;
 
 import static com.habittracker.api.config.constants.JwtTestConstant.AUTHORIZATION_HEADER;
 import static com.habittracker.api.config.constants.JwtTestConstant.BEARER_PREFIX;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -34,5 +35,30 @@ public class MockMvcTestUtils {
   public static MockHttpServletRequestBuilder addJwt(
       String jwt, MockHttpServletRequestBuilder builder) {
     return builder.header(AUTHORIZATION_HEADER, BEARER_PREFIX + jwt);
+  }
+
+  public <T> ResultActions performAuthenticatedPostRequest(String endpoint, T body, String authToken) throws Exception {
+    return mockMvc.perform(
+        post(endpoint)
+            .header("Authorization", authToken)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(body)));
+  }
+
+  public ResultActions performAuthenticatedGetRequest(String endpoint, String authToken) throws Exception {
+    return mockMvc.perform(
+        get(endpoint)
+            .header("Authorization", authToken));
+  }
+
+  public <T> ResultActions performUnauthenticatedPostRequest(String endpoint, T body) throws Exception {
+    return mockMvc.perform(
+        post(endpoint)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(body)));
+  }
+
+  public ResultActions performUnauthenticatedGetRequest(String endpoint) throws Exception {
+    return mockMvc.perform(get(endpoint));
   }
 }
