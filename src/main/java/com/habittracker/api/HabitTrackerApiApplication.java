28c0ecbeb8f6b733791f48233f822022689133b2
Shenol Shengov <shenolshengov41@gmail.com>
2025-09-13 19:01:52 +0300

    feat: add pageable dto serialization

--- src/main/java/com/habittracker/api/HabitTrackerApiApplication.java
+++ src/main/java/com/habittracker/api/HabitTrackerApiApplication.java
@@ -5,12 +5,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @EnableCaching
 @EnableScheduling
 @EnableConfigurationProperties(JwtProperties.class)
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class HabitTrackerApiApplication {
 
   public static void main(String[] args) {
