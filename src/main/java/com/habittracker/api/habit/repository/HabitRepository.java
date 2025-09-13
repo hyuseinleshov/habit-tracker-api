5c8bbd2b15b036b94a8fc4825dc2979e34d6bb4c
Shenol Shengov <shenolshengov41@gmail.com>
2025-09-13 20:31:05 +0300

    feat: add jpa specification executor to habit repository

--- src/main/java/com/habittracker/api/habit/repository/HabitRepository.java
+++ src/main/java/com/habittracker/api/habit/repository/HabitRepository.java
@@ -5,11 +5,12 @@ import com.habittracker.api.habit.model.HabitEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.time.Instant;
 import java.util.UUID;
 
-public interface HabitRepository extends JpaRepository<HabitEntity, UUID> {
+public interface HabitRepository extends JpaRepository<HabitEntity, UUID>, JpaSpecificationExecutor<HabitEntity> {
 
   boolean existsByUserAndNameIgnoreCase(UserEntity user, String name);
 
