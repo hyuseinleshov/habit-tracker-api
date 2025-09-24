b1682b257e1b13b1c696fe88c2c8ae763b350378
Shenol Shengov <shenolshengov41@gmail.com>
2025-09-24 16:15:53 +0300

    feat: configure habit mapper and add update method

--- src/main/java/com/habittracker/api/habit/mapper/HabitMapper.java
+++ src/main/java/com/habittracker/api/habit/mapper/HabitMapper.java
@@ -1,15 +1,20 @@
 package com.habittracker.api.habit.mapper;
 
 import com.habittracker.api.habit.dto.HabitResponse;
+import com.habittracker.api.habit.dto.UpdateHabitRequest;
 import com.habittracker.api.habit.model.HabitEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface HabitMapper {
 
   @Mapping(
       target = "frequency",
       expression = "java(com.habittracker.api.habit.model.Frequency.DAILY)")
   HabitResponse toResponse(HabitEntity entity);
+
+  HabitEntity updateHabitFromUpdateRequest(UpdateHabitRequest updateHabitRequest, @MappingTarget HabitEntity habit);
 }
