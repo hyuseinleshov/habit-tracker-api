08380d9c1efdcbfb7e12ffefa685f99de58c2e99
Shenol Shengov <shenolshengov41@gmail.com>
2025-09-24 15:58:35 +0300

    feat: add update habit request

---
+++ src/main/java/com/habittracker/api/habit/dto/UpdateHabitRequest.java
@@ -0,0 +1,9 @@
+package com.habittracker.api.habit.dto;
+
+import static com.habittracker.api.habit.constants.HabitConstants.*;
+
+import org.hibernate.validator.constraints.Length;
+
+public record UpdateHabitRequest(
+    @Length(max = 100, message = NAME_LENGTH_MESSAGE) String name,
+    @Length(max = 2000, message = DESCRIPTION_LENGTH_MESSAGE) String description) {}
