425c70a8d20f60694f1d7e007a81d50e4614da09
Shenol Shengov <shenolshengov41@gmail.com>
2025-09-26 17:45:31 +0300

    feat: add daily check in service

---
+++ src/main/java/com/habittracker/api/checkin/service/DailyCheckInService.java
@@ -0,0 +1,10 @@
+package com.habittracker.api.checkin.service;
+
+import com.habittracker.api.habit.model.HabitEntity;
+
+import java.time.ZoneId;
+
+public interface DailyCheckInService {
+
+    void registerTodayCheckin(HabitEntity habit, ZoneId userTimeZone);
+}
