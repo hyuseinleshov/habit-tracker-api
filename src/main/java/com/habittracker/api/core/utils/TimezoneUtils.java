8b9e60ea005cc69c98e178acc0278f36e27bfe55
Shenol Shengov <shenolshengov41@gmail.com>
2025-09-26 17:32:07 +0300

    feat: add utils method to calculate duration until midnight

--- src/main/java/com/habittracker/api/core/utils/TimezoneUtils.java
+++ src/main/java/com/habittracker/api/core/utils/TimezoneUtils.java
@@ -1,6 +1,9 @@
 package com.habittracker.api.core.utils;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import java.util.Set;
 
 public final class TimezoneUtils {
@@ -12,5 +15,12 @@ public final class TimezoneUtils {
   public static boolean isValidTimezone(String timezone) {
     if (timezone == null) return false;
     return VALID_ZONE_IDS.contains(timezone.trim());
+  }
+
+  public static Duration calculateDurationUntilMidnight(ZoneId userTimeZone) {
+    LocalDateTime now = LocalDateTime.now(userTimeZone);
+    LocalDateTime midnight = now.plusDays(1).truncatedTo(ChronoUnit.DAYS);
+    System.out.println(now.truncatedTo(ChronoUnit.MINUTES));
+    return Duration.between(now, midnight);
   }
 }
