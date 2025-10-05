4f69d99bb466889a237a16ba8db04aea02a3002d
hyuseinleshov <hyusein.leshov@gmail.com>
2025-10-05 12:00:42 +0300

    feat: add CheckInNotFoundException and error constant

--- src/main/java/com/habittracker/api/checkin/constants/CheckInConstants.java
+++ src/main/java/com/habittracker/api/checkin/constants/CheckInConstants.java
@@ -4,4 +4,5 @@ public class CheckInConstants {
 
   public static final String DUPLICATION_CHECK_IN_MESSAGE =
       "A check-in for habit with id %s already exists for today.";
+  public static final String CHECK_IN_NOT_FOUND_MESSAGE = "Check-in not found.";
 }
