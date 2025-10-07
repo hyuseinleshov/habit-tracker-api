9ef016d32ad005c99eb626ac0f99b87cd1f93830
hyuseinleshov <hyusein.leshov@gmail.com>
2025-10-07 10:39:10 +0300

    feat: add StreakConstants for cache key prefix

---
+++ src/main/java/com/habittracker/api/checkin/constants/StreakConstants.java
@@ -0,0 +1,10 @@
+package com.habittracker.api.checkin.constants;
+
+public final class StreakConstants {
+
+  public static final String STREAK_CACHE_KEY_PREFIX = "streak:";
+
+  private StreakConstants() {
+    throw new UnsupportedOperationException("Utility class, do not instantiate");
+  }
+}
