ff3f5fed3f0141ae1e4be0e5ee765c3f516824d4
Shenol Shengov <shenolshengov41@gmail.com>
2025-10-16 21:28:20 +0300

    feat: add habit status project and method to get it

---
+++ src/main/java/com/habittracker/api/habit/projections/HabitStatusProjection.java
@@ -0,0 +1,9 @@
+package com.habittracker.api.habit.projections;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public interface HabitStatusProjection {
+    UUID getId();
+    Instant deletedAt();
+}
--- src/main/java/com/habittracker/api/habit/repository/HabitRepository.java
+++ src/main/java/com/habittracker/api/habit/repository/HabitRepository.java
@@ -2,12 +2,17 @@ package com.habittracker.api.habit.repository;
 
 import com.habittracker.api.habit.model.HabitEntity;
 import java.time.Instant;
+import java.util.Optional;
 import java.util.UUID;
+
+import com.habittracker.api.habit.projections.HabitStatusProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface HabitRepository
     extends JpaRepository<HabitEntity, UUID>, JpaSpecificationExecutor<HabitEntity> {
+
+  Optional<HabitStatusProjection> findStatusById(UUID id);
 
   boolean existsByNameIgnoreCaseAndUserId(String name, UUID userId);
 
