0bbc561af887e9ddd2bb79cf4a694cb99d00047c
Shenol Shengov <shenolshengov41@gmail.com>
2025-10-18 16:17:31 +0300

    feat: add auth check in AuthUtils

--- src/main/java/com/habittracker/api/auth/utils/AuthUtils.java
+++ src/main/java/com/habittracker/api/auth/utils/AuthUtils.java
@@ -1,9 +1,11 @@
 package com.habittracker.api.auth.utils;
 
+import static com.habittracker.api.core.exception.ExceptionConstants.ILLEGAL_STATE_MESSAGE;
 import static com.habittracker.api.core.utils.TimeZoneUtils.parseTimeZone;
 
 import com.habittracker.api.auth.model.UserDetailsImpl;
 import java.time.ZoneId;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public final class AuthUtils {
@@ -13,6 +15,10 @@ public final class AuthUtils {
   }
 
   private static UserDetailsImpl getDetails() {
-    return (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    if (authentication == null || !(authentication.getPrincipal() instanceof UserDetailsImpl)) {
+      throw new IllegalStateException(ILLEGAL_STATE_MESSAGE);
+    }
+    return (UserDetailsImpl) authentication.getPrincipal();
   }
 }
