f1d172c28b335f7a09245690d0dbd5e8b077cd36
Shenol Shengov <shenolshengov41@gmail.com>
2025-10-19 22:45:16 +0300

    feat: implement calculateStatics method

--- src/main/java/com/habittracker/api/habit/service/StatisticsService.java
+++ src/main/java/com/habittracker/api/habit/service/StatisticsService.java
@@ -1,11 +1,9 @@
 package com.habittracker.api.habit.service;
 
 import com.habittracker.api.habit.dto.HabitStatisticResponse;
-
 import java.util.UUID;
 
 public interface StatisticsService {
 
-    HabitStatisticResponse calculateStatistics(UUID habitId);
-
+  HabitStatisticResponse calculateStatistics(UUID habitId);
 }
---
+++ src/main/java/com/habittracker/api/habit/service/impl/StatisticsServiceImpl.java
@@ -0,0 +1,45 @@
+package com.habittracker.api.habit.service.impl;
+
+import com.habittracker.api.checkin.service.CheckInService;
+import com.habittracker.api.checkin.service.StreakService;
+import com.habittracker.api.habit.dto.HabitStatisticResponse;
+import com.habittracker.api.habit.helpers.HabitHelper;
+import com.habittracker.api.habit.model.HabitEntity;
+import com.habittracker.api.habit.service.StatisticsService;
+import java.time.LocalDate;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class StatisticsServiceImpl implements StatisticsService {
+
+  private final HabitHelper habitHelper;
+  private final CheckInService checkInService;
+  private final StreakService streakService;
+
+  @Override
+  @Transactional(readOnly = true)
+  @PreAuthorize("@habitHelper.isOwnedByUser(#habitId, principal.id)")
+  public HabitStatisticResponse calculateStatistics(UUID habitId) {
+    HabitEntity habit = habitHelper.getNotDeletedOrThrow(habitId);
+    long totalCheckins = checkInService.getCheckInsCount(habitId);
+    int currentStreak = streakService.calculateStreak(habitId).currentStreak();
+    LocalDate lastCheckInDate = checkInService.getLastCheckInDate(habitId);
+    return new HabitStatisticResponse(
+        habitId,
+        habit.getName(),
+        totalCheckins,
+        new HabitStatisticResponse.StreakData(currentStreak, buildBestSteakData(habit)),
+        lastCheckInDate);
+  }
+
+  private HabitStatisticResponse.BestStreakData buildBestSteakData(HabitEntity habit) {
+    LocalDate startDate = habit.getBestStreakStartDate();
+    LocalDate endDate = startDate != null ? startDate.plusDays(habit.getBestStreak()) : null;
+    return new HabitStatisticResponse.BestStreakData(habit.getBestStreak(), startDate, endDate);
+  }
+}
