50986706a9c2f946c21120cf3c00f52c9f94a558
Shenol Shengov <shenolshengov41@gmail.com>
2025-10-19 18:06:18 +0300

    feat: add StatisticsService interface

---
+++ src/main/java/com/habittracker/api/habit/service/StatisticsService.java
@@ -0,0 +1,11 @@
+package com.habittracker.api.habit.service;
+
+import com.habittracker.api.habit.dto.HabitStatisticResponse;
+
+import java.util.UUID;
+
+public interface StatisticsService {
+
+    HabitStatisticResponse calculateStatistics(UUID habitId);
+
+}
