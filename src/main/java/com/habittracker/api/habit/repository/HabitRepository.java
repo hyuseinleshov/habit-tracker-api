51e2cfc0a6d5e2a17a6467308cd064c6d67bee7d
Shenol Shengov <shenolshengov41@gmail.com>
2025-10-26 23:17:18 +0200

    fix: change way getting user best habit

--- src/main/java/com/habittracker/api/habit/repository/HabitRepository.java
+++ src/main/java/com/habittracker/api/habit/repository/HabitRepository.java
@@ -5,13 +5,21 @@ import com.habittracker.api.habit.projections.HabitStatusProjection;
 import java.time.Instant;
 import java.util.Optional;
 import java.util.UUID;
+
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 public interface HabitRepository
     extends JpaRepository<HabitEntity, UUID>, JpaSpecificationExecutor<HabitEntity> {
 
   Optional<HabitStatusProjection> findStatusById(UUID id);
+
+  @Query("SELECT h from HabitEntity h WHERE h.user.id = :userId " +
+          "AND h.deletedAt IS NULL ORDER BY h.bestStreak DESC " +
+          "LIMIT 1")
+  Optional<HabitEntity> findBestStreakByUserId(@Param("userId") UUID userId);
 
   boolean existsByNameIgnoreCaseAndUserId(String name, UUID userId);
 
--- src/main/java/com/habittracker/api/user/service/impl/UserStatisticsServiceImpl.java
+++ src/main/java/com/habittracker/api/user/service/impl/UserStatisticsServiceImpl.java
@@ -1,18 +1,20 @@
-package com.habittracker.api.user.service;
+package com.habittracker.api.user.service.impl;
 
 import com.habittracker.api.auth.model.UserEntity;
 import com.habittracker.api.checkin.service.CheckInService;
 import com.habittracker.api.checkin.service.StreakService;
 import com.habittracker.api.habit.dto.HabitStatisticResponse;
-import com.habittracker.api.habit.model.HabitEntity;
+import com.habittracker.api.habit.repository.HabitRepository;
 import com.habittracker.api.user.dto.UserStatisticsResponse;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.util.Comparator;
+import com.habittracker.api.user.service.UserStatisticsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -20,23 +22,23 @@ public class UserStatisticsServiceImpl implements UserStatisticsService {
 
   private final CheckInService checkInService;
   private final StreakService streakService;
+  private final HabitRepository habitRepository;
 
   @Override
   @Transactional(readOnly = true)
   @Cacheable(value = "userStatistics", key = "#user.id")
   public UserStatisticsResponse calculateStatistics(UserEntity user) {
     long totalCheckIns = checkInService.getUserCheckInsCount(user.getId());
-    HabitStatisticResponse.BestStreakData userBestStreak = getUserBestStreak(user);
+    HabitStatisticResponse.BestStreakData userBestStreak = getUserBestStreak(user.getId());
     long activeStreaks = streakService.getUserActiveStreaks(user.getId());
     LocalDate lastCheckInDate = checkInService.getUserLastCheckInDate(user.getId());
     return new UserStatisticsResponse(
         user.getId(), totalCheckIns, userBestStreak, activeStreaks, lastCheckInDate, Instant.now());
   }
 
-  private static HabitStatisticResponse.BestStreakData getUserBestStreak(UserEntity user) {
-    return user.getHabits().stream()
-        .max(Comparator.comparing(HabitEntity::getBestStreak))
-        .map(
+  private HabitStatisticResponse.BestStreakData getUserBestStreak(UUID userId) {
+    return habitRepository.findBestStreakByUserId(userId)
+            .map(
             habit ->
                 HabitStatisticResponse.BestStreakData.of(
                     habit.getBestStreak(), habit.getBestStreakStartDate(), habit.getId()))
