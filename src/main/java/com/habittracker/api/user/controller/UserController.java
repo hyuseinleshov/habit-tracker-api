bb7ca0ef42ad285c75d9481c3c031c6d72a4462b
Shenol Shengov <shenolshengov41@gmail.com>
2025-10-31 23:11:57 +0200

    feat: update refresh token cookie on update profile and clear it on delete profile

--- src/main/java/com/habittracker/api/user/controller/UserController.java
+++ src/main/java/com/habittracker/api/user/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.habittracker.api.user.controller;
 
 import com.habittracker.api.auth.model.UserDetailsImpl;
+import com.habittracker.api.auth.service.RefreshTokenService;
+import com.habittracker.api.auth.utils.RefreshTokenCookieUtils;
 import com.habittracker.api.user.dto.UserProfileDTO;
 import com.habittracker.api.user.service.UserProfileService;
 import com.habittracker.api.user.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +20,8 @@ public class UserController {
 
   private final UserProfileService userProfileService;
   private final UserService userService;
+  private final RefreshTokenService refreshTokenService;
+  private final RefreshTokenCookieUtils refreshTokenCookieUtils;
 
   @GetMapping
   public ResponseEntity<UserProfileDTO> get(@AuthenticationPrincipal UserDetailsImpl principal) {
@@ -26,13 +31,20 @@ public class UserController {
   @PutMapping
   public ResponseEntity<UserProfileDTO> update(
       @RequestBody @Valid UserProfileDTO userProfileDTO,
-      @AuthenticationPrincipal UserDetailsImpl principal) {
-    return ResponseEntity.ok(userProfileService.update(principal.id(), userProfileDTO));
+      @AuthenticationPrincipal UserDetailsImpl principal,
+      HttpServletResponse httpResponse) {
+    UserProfileDTO response = userProfileService.update(principal.id(), userProfileDTO);
+    refreshTokenService.revokeAllRefreshTokensForUser(principal.id());
+    refreshTokenCookieUtils.addRefreshTokenCookie(
+        refreshTokenService.createRefreshToken(principal.id()), httpResponse);
+    return ResponseEntity.ok(response);
   }
 
   @DeleteMapping
-  public ResponseEntity<Void> delete(@AuthenticationPrincipal UserDetailsImpl principal) {
+  public ResponseEntity<Void> delete(
+      @AuthenticationPrincipal UserDetailsImpl principal, HttpServletResponse httpResponse) {
     userService.delete(principal.id());
+    refreshTokenCookieUtils.clearRefreshTokenCookie(httpResponse);
     return ResponseEntity.noContent().build();
   }
 }
